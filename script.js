1b0005c344242a8eed22cd5a357ebcf0c3f59254
Vincent <vmgboyles@gmail.com>
2026-05-12 10:58:10 +0800

    added basic arithmetic functions and operate function to evaluate only two numbers

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 const button = document.querySelector('#button-container')
 const content = document.querySelector('.content')
 
-content.textContent = 0;
+content.textContent = '';
+
+const operandObj = {
+    operandOne = 0,
+    operandTwo = 0,
+}
+
 
 button.addEventListener('click', (event) => {
     let target = event.target
-    content.textContent += target.id
+
+    switch(target.id) {
+        case 'clear':
+            content.textContent = ''
+            break
+        case 'back':
+            content.textContent = ''
+            break
+        default:
+            content.textContent += target.id
+    }
 })
+
+
+
+
+let operand = {
+  one: 4,
+  two: 3,
+}
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (operator, num1, num2) => operator(num1, num2)
+
+console.log(operate(add, operand.one, operand.two))
+
