351e8a6fe3fa94e95243b4c3c9ba563cd47a6133
Vincent <vmgboyles@gmail.com>
2026-05-13 17:58:15 +0800

    Added functionality in the operator events where a basic arithmetic will work for only a pair of numbers

--- script.js
+++ script.js
@@ -7,37 +7,53 @@ display.textContent = '';
 const user = {
   num1: 0,
   num2: 0,
-  operator: function(){},
+  operator: () => {},
 }
 
+const operate = (operator, num1, num2) => operator(num1, num2)
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-const operate = (operator, num1, num2) => operator(num1, num2)
-const zeroDivision = (num1, num2) => {
+const divide = (num1, num2) => {
     if (num1 === 0 || num2 === 0) {
         display.textContent = 'MATH ERROR'
         return
+    } else {
+        return num1 / num2
     }
 } 
 
 
+let holdValue = ''
+let switchNum = 0
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click', (event) => {
         switch(event.target.id) {
             case 'add':
+                switchNum = 1
+                holdValue = ''
+                user.operator = add;
                 break
             case 'subtract':
+                switchNum = 1
+                holdValue = ''
+                user.operator = subtract;
                 break
             case 'multiply':
+                switchNum = 1
+                holdValue = ''
+                user.operator = multiply;
                 break
             case 'divide':
-                zeroDivision(user.num1, user.num2)
+                switchNum = 1
+                holdValue = ''
+                user.operator = divide;
                 break
             case 'decimal':
                 break
             case 'equals':
+                let result = operate(user.operator, user.num1, user.num2)
+                display.textContent = result
                 break
         }
     })
@@ -45,9 +61,20 @@ for (let i = 0; i < operators.length; i++) {
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', (event) => {
-        display.textContent += event.target.id
+        holdValue += event.target.id
+        display.textContent = holdValue
+            if (switchNum === 0) {
+        user.num1 = +display.textContent
+    } else {
+        user.num2 = +display.textContent
+    }
     })
 }
+
+
+
+
+
 
 
 // button.addEventListener('click', (event) => {
